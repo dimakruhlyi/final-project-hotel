5350e4818ea28163e97d9e1c164b261416a8b7ae
Dima Kruhlyi <dimakruhly@gmail.com>
2020-01-18 02:15:22 +0200

    created pagination

--- js/apartments.js
+++ js/apartments.js
@@ -20,16 +20,17 @@ function readTextFile(file, callback) {
 readTextFile("../js/data/apartmentsData.json", function(text){
   let data = JSON.parse(text);
   //console.log(data);
-  showCard(data);
-  showDataPagination();
   
+  let count_loots = showCard(data);
+  showDataPagination(count_loots);
+  //console.log(count_loots);
   
 });
 
+
  function showCard(apartments){
   let out = '';
-  let counter = 1;
-  //let bedsArray = localStorage.getItem('apartments-beds') ? JSON.parse(localStorage.getItem('apartments-beds')) : [];
+  let counter = 0;
   let bedsArray = [], imgArray = [],titleArray = [], priceArray = [];
   for(let key in apartments){
     imgArray.push(apartments[key].img);
@@ -57,59 +58,60 @@ readTextFile("../js/data/apartmentsData.json", function(text){
     counter++;
   }
   document.getElementById('apartments-data').innerHTML = out;
+  return counter;
  }
  
 
 /*------------------------------------------ Pagination ------------------------------------------*/
-function showDataPagination(){
-  let count = 6; //всего записей
-  let cnt = 2; //сколько отображаем сначала
-  let cnt_page = Math.ceil(count / cnt); //кол-во страниц
+function showDataPagination(count_loots){
+    let count = count_loots; 
+    let cnt = 3; 
+    let cnt_page = Math.ceil(count / cnt); 
 
-  //выводим список страниц
-  let paginator = document.querySelector(".pagination-block");
-  let page = "";
-  for (let i = 0; i < cnt_page; i++) {
-    page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
-  }
-  paginator.innerHTML = page;
+    //show pages list
+    let paginator = document.querySelector(".pagination-block");
+    let page = "";
+    for (let i = 0; i < cnt_page; i++) {
+      page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+    }
+    paginator.innerHTML = page;
 
-  //выводим первые записи {cnt}
-  let div_num = document.querySelectorAll(".border");
-  for (let i = 0; i < div_num.length; i++) {
-    if (i < cnt) {
+    //show data loots
+    let div_num = document.querySelectorAll(".border");
+    for (let i = 0; i < div_num.length; i++) {
+      if (i < cnt) {
+        div_num[i].style.display = "block";
+      }
+    }
+
+    let main_page = document.getElementById("page1");
+    main_page.classList.add("paginator_active");
+
+  //next page with loots
+  document.getElementById('pagination-block').onclick =  function (event) {
+    let e = event || window.event;
+    let target = e.target;
+    let id = target.id;
+    
+    if (target.tagName.toLowerCase() != "span") return;
+    
+    let num_ = id.substr(4);
+    let data_page = +target.dataset.page;
+    main_page.classList.remove("paginator_active");
+    main_page = document.getElementById(id);
+    main_page.classList.add("paginator_active");
+
+    let j = 0;
+    for (let i = 0; i < div_num.length; i++) {
+      let data_num = div_num[i].dataset.num;
+      if (data_num <= data_page || data_num >= data_page)
+        div_num[i].style.display = "none";
+
+    }
+    for (let i = data_page; i < div_num.length; i++) {
+      if (j >= cnt) break;
       div_num[i].style.display = "block";
+      j++;
     }
   }
-
-  let main_page = document.getElementById("page1");
-  main_page.classList.add("paginator_active");
 }
-//листаем
-function pagination(event) {
-  let e = event || window.event;
-  let target = e.target;
-  let id = target.id;
-  
-  if (target.tagName.toLowerCase() != "span") return;
-  
-  let num_ = id.substr(4);
-  let data_page = +target.dataset.page;
-  main_page.classList.remove("paginator_active");
-  main_page = document.getElementById(id);
-  main_page.classList.add("paginator_active");
-
-  let j = 0;
-  for (let i = 0; i < div_num.length; i++) {
-    let data_num = div_num[i].dataset.num;
-    if (data_num <= data_page || data_num >= data_page)
-      div_num[i].style.display = "none";
-
-  }
-  for (let i = data_page; i < div_num.length; i++) {
-    if (j >= cnt) break;
-    div_num[i].style.display = "block";
-    j++;
-  }
-}
-
